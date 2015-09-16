c05d9983257213d2d086fe8b3cb755ea4f0003e0
Joel Burdette <joelburdette04@gmail.com>
2015-09-16 10:01:44 -0500

    Create namespace3.cpp

---
+++ CS1/namespace3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+ //using namespace std;
+int main() {
+using std::cout;   
+using std::rand;
+using std::endl;   
+ 
+
+ int i;
+
+        i=rand();
+
+        cout<<i<<endl;
+
+        return 0;
+}
