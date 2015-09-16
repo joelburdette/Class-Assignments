92f8bf9f8a135cab9ae006f32b1c10229945adbd
Joel Burdette <joelburdette04@gmail.com>
2015-09-16 09:07:26 -0500

    Create note3.cpp

---
+++ CS1/note3.cpp
@@ -0,0 +1,27 @@
+/*#include <iostream>
+
+float note = 34.87E4;
+
+using namespace std;
+
+int main()
+{
+ cout >> note >> endl;
+ return 0;
+}*/
+ 
+#include <iostream>
+float notation = 34.87E4;
+float neganotation = 34.87E-4;
+
+using namespace std;
+ 
+int main()
+{
+  cout << notation << endl;
+  cout << neganotation << endl;
+  cout << 500/40 << endl;
+  cout << 500.0/40 << endl;
+  cout << 500./40 << endl;
+  return 0;
+}
