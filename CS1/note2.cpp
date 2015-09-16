1fc076da2fb24ce3671380de57ba50c9f0d2fa25
Joel Burdette <joelburdette04@gmail.com>
2015-09-16 09:06:43 -0500

    Create note2.cpp

---
+++ CS1/note2.cpp
@@ -0,0 +1,26 @@
+/*#include <iostream>
+
+float note = 34.87E4;
+
+using namespace std;
+
+int main()
+{
+ cout >> note >> endl;
+ return 0;
+}*/
+
+// 'Hello World!' program 
+ 
+#include <iostream>
+float notation = 34.87E4;
+float neganotation = 34.87E-4;
+
+using namespace std;
+ 
+int main()
+{
+  cout << notation << endl;
+  cout << neganotation << endl;
+  return 0;
+}
