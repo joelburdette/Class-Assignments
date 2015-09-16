4fb69820b157a69aa6828ed12b8a964ebf19d008
Joel Burdette <joelburdette04@gmail.com>
2015-09-16 09:09:13 -0500

    Create length.cpp

---
+++ CS1/length.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+
+
+using namespace std;
+
+int main()
+{ 
+	string something;
+	cout << "please enter your input" << endl;
+	cin >> something;  
+	cout << something.length();   
+	  return 0;
+}
